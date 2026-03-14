3ea2990bb144391740c0f2aa211feafc2abecd7b
‪mahmoud sameh <mahmod48873@gmail.com>
2026-03-15 00:25:31 +0200

    new

--- src/components/header/BtmHeader.jsx
+++ src/components/header/BtmHeader.jsx
@@ -1,6 +1,23 @@
 
+import { IoMdMenu } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 export default function BtmHeader() {
+
+
   return (
-    <div></div>
-  )
+    <div className="btm_header">
+      <div className="container">
+        <nav className="nav">
+          <div className="category_nav">
+            <div className="category_btm">
+              <IoMdMenu />
+              <p>list</p>
+              <IoIosArrowDown />
+            </div>
+            <div className="category_nav_list"> </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
